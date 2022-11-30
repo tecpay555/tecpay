266061b194304b62bb459f8b4369c71e9d2103a8
tecpay555 <118281547+tecpay555@users.noreply.github.com>
2022-11-30 23:56:32 +0530

    Update webhook.php

--- webhook/php/webhook.php
+++ webhook/php/webhook.php
@@ -8,10 +8,13 @@ $amount = $_POST['amount'];
 $status = $_POST['status'];
 $post_hash = $_POST['post_hash'];
 
+
+$encrypted_hash=base64_decode($post_hash);
+$remote_hash=decrypt($encrypted_hash, $secret_key);
 // Compute the payment hash locally
 $local_hash = md5($order_id.$amount.$status.$secret_key);
 
-if ($post_hash == $local_hash) {
+if ($remote_hash == $local_hash) {
   // Mark the transaction as success & process the order
   // You can write code process the order herer
   $hash_status = "Hash Matched";
@@ -27,4 +30,18 @@ $data['hash_status']=$hash_status;
 $data['acknowledge']=$acknowledge;
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);  
+
+
+function decrypt($ivHashCiphertext, $password) {
+    $method = "AES-256-CBC";
+    $iv = substr($ivHashCiphertext, 0, 16);
+    $hash = substr($ivHashCiphertext, 16, 32);
+    $ciphertext = substr($ivHashCiphertext, 48);
+    $key = hash('sha256', $password, true);
+
+    if (!hash_equals(hash_hmac('sha256', $ciphertext . $iv, $key, true), $hash)) return null;
+    return openssl_decrypt($ciphertext, $method, $key, OPENSSL_RAW_DATA, $iv);
+}
+
+
 ?>
